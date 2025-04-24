5a8665032ae46926d2075688f7c0b3a543efe344
Alisson <aliba.truta@gmail.com>
2025-04-24 20:59:41 -0300

    calculadora de formas

---
+++ exercicios.nivel.one/exercicios/src/quadrado/calculadora/CalculadoraForma.java
@@ -0,0 +1,12 @@
+package quadrado.calculadora;
+
+public class CalculadoraForma {
+    public static void main(String[] args) {
+        int quadrado = 4;
+        double tamanho = 2.5;
+
+        double areaTotal = quadrado * tamanho;
+
+        System.out.println("\nEste quadrado tem um total de " + areaTotal + " metros lineares");
+    }
+}
